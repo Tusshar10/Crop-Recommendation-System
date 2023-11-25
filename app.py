0ce9c45baadde87abf5293e3c311194623cd2e33
Tusshar10 <tusshargupta100@gmail.com>
2023-11-25 11:32:14 +0530

    Added recommended fertilizer and other stats

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def index():
         return render_template("index.html")
     else:
         crop={}
+        fert = {"rice": "20:10:10", "maize": "4:2:1", "chickpea": "2:3:4", "kidneybeans": "1:2:1", "pigeonpeas": "1:2:1",
+        "mothbeans": "1:2:1", "mungbean": "1:2:1", "blackgram": "2:3:1", "lentil": "1:2:1", "pomegranate": "2:1:4",
+        "banana": "4:3:2", "mango": "2:2:3", "grapes": "1:1:1", "watermelon": "2:1:1", "muskmelon": "1:2:4",
+        "apple": "1:1:1", "orange": "2:1:1", "papaya": "1:1:1", "coconut": "2:1:3", "cotton": "1:3:2", "jute": "2:1:1",
+        "coffee": "1:1:1"}
         crop['N'] = request.form['nitrogen']
         crop['P'] = request.form['phosphorus']
         crop['K'] = request.form['potassium']
@@ -27,7 +32,7 @@ def index():
         df = pd.DataFrame(data, columns=feature_names)
         predict = model.predict(df)
         crop['name']=predict[0]
-        return render_template("crop.html",crop=crop)
+        return render_template("crop.html",crop=crop,fert=fert)
 
 if __name__=="__main__":
     app.run(debug=True)
