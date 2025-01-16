78ed492bf10c9e2889f361af535ac36a49fcb7da
Tusshar10 <tusshargupta100@gmail.com>
2025-01-16 23:29:21 +0530

    Created .env file

--- utils/humtem.py
+++ utils/humtem.py
@@ -1,14 +1,19 @@
 import requests
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 def get_temp_hum(district, state=None, month=None):
+    api_key = os.getenv("OPENWEATHER_API_KEY")
+    if not api_key:
+        raise Exception("API key not found. Please set it in the .env file.")
     # First, try fetching data using district
-    district_url = f"https://api.openweathermap.org/data/2.5/weather?q={district}&appid=af1253e5ac93e4757ad434376e322761"
+    district_url = f"https://api.openweathermap.org/data/2.5/weather?q={district}&appid={api_key}"
     district_response = requests.get(district_url)
 
     # If the request using district fails, try using state
     if district_response.status_code != 200:
         if state is not None:
-            state_url = f"https://api.openweathermap.org/data/2.5/weather?q={state}&appid=af1253e5ac93e4757ad434376e322761"
+            state_url = f"https://api.openweathermap.org/data/2.5/weather?q={state}&appid={api_key}"
             state_response = requests.get(state_url)
 
             if state_response.status_code != 200:
